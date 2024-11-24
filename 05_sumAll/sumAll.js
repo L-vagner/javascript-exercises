a3b06d00bb06aa3410e228f727d820a063216dbb
Ludovic Vagner <ludovicvagner@yahoo.fr>
2024-11-24 11:15:36 +0100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    for (let i = 0 , len = arguments.length; i < len; i ++)
+    {
+        if ((arguments[i] < 0) || (!Number.isInteger(arguments[i])) || (typeof arguments[i] != "number"))
+        {
+            return 'ERROR';
+        }
+    }
+    let start;
+    let end;
+    let sum = 0;
+if (Math.min(numA, numB) == numA){
+    start = numA;
+    end = numB;
+} else {
+    start = numB;
+    end = numA
+}
 
+for (start; start <= end; start ++)
+{
+ sum += start;
+}
+return sum;
 };
 
 // Do not edit below this line
