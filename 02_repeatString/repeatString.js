b3993eb2cbe37a6ba57636f0b85a7c350102b541
Ludovic Vagner <ludovicvagner@yahoo.fr>
2024-11-24 10:42:37 +0100

    finish exercise 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function( word,n) {
+    let val = "";
+    if (n < 0)
+    {
+        return "ERROR";
+    }
+for (let i = 0; i < n; i++)
+val +=word;
+return val;
 };
 
 // Do not edit below this line
