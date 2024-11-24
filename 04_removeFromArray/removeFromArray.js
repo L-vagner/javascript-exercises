04e09957d9e6f33127519017d2d9c43dcbe896d2
Ludovic Vagner <ludovicvagner@yahoo.fr>
2024-11-24 11:02:37 +0100

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    
+    for (const item of items){
+        while (array.includes(item)) {
+            let i = array.indexOf(item);
+            array.splice(i, 1);
+        }
+    }
+    return array;
+        
 };
 
 // Do not edit below this line
